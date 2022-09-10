ebca3c9ea9753992c3c4fb89fd14954c62033f39
Amal Prasad <amalprasad354@gmail.com>
2022-09-10 08:51:22 +0530

    error pointed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -14,7 +14,7 @@ function Contact() {
         <h4 className='text-center next'  styel={{paddingTop:'80px'}}><span style={{color:"#5bdb94"}}>03.</span>WHAT'S NEXT---</h4>
         <h1 className='text-center'>Get in Touch</h1>
         <p className='text-center txt'>I'm currently looking for new opportunities, my inbox is always open. <br /> Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
-        <button  className='btn btn-me m-5 '> <a href="mailto:amalprasad@gmail.com" style={{textDecoration:"none",textDecorationColor:"antiquewhite"}}>Say Hello!✉️</a></button>
+        <button  className='btn btn-me m-5 '> <a href="mailto:amalprasad354@gmail.com" style={{textDecoration:"none",textDecorationColor:"antiquewhite"}}>Say Hello!✉️</a></button>
         </Col>
       </Row>
     </Container>
